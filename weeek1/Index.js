240738b27e59e359d4c380f548e06bd51d143966
Arnav10008 <pandeyarnav978@gmail.com>
2025-10-09 16:43:11 +0000

    addex

--- weeek1/Index.js
+++ weeek1/Index.js
@@ -1,1 +1,8 @@
-
+console.log(" This is my first programme")
+console.log("welcome john ,your monthly salary is 50000")
+const num1 =5;
+const num2 =3;
+// add two numbers
+const sum =num1 + num2
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum)
